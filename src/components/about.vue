4e15a0141f9bbd30b6fcc451709345989b3016af
zhuhl3635 <zhuhl3635@163.com>
2018-07-25 18:18:30 +0800

    nuxt 与 vue  keep-alive的不同

--- src/components/about.vue
+++ src/components/about.vue
@@ -6,14 +6,24 @@
           <router-link :to="{path:'/child', query: {id: index}}">关于我们{{index}}</router-link>
       </li>
     </ul>
+    <input type="text" v-model="inputVal">
   </div>
   <!-- </keep-alive> -->
 </template>
 <script>
 export default {
   name: 'about-component',
+  data() {
+    return {
+      inputVal: '123'
+    };
+  },
   created() {
     console.log('about --- keepalive');
+  },
+  activated() {
+    // keep-alive 才能触发这个钩子
+    console.log(this.inputVal);
   }
 };
 </script>
